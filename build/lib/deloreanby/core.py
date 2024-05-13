8711446b9a7b4cc7c0044653d155e6509987856e
KirbyHacks <kirbywtfofficial@gmail.com>
2024-05-12 22:47:52 -0300

    wasd

--- build/lib/deloreanby/core.py
+++ build/lib/deloreanby/core.py
@@ -3,13 +3,15 @@ from urllib.parse import quote
 import json
 
 class DeloreanPremium():
-    async def get(self, *, url, api_key):
+    async def get(self, *, url, api_key, advanced_mode=False):
         async with aiohttp.ClientSession() as session:
             async with session.get(f"https://dlr-api.woozym.workers.dev/?url={quote(url)}", headers={"x-api-key": api_key}) as response:
                 if response.status != 200:
                     return await response.text()
                 try:
-                    r = json.loads(await response.text())
+                    r = await response.json()
+                    if advanced_mode:
+                        return r
                     return r["result"]
                 except Exception as e:
                     print(e)
@@ -22,9 +24,23 @@ class DeloreanFree():
                 if response.status != 200:
                     return await response.text()
                 try:
-                    r = json.loads(await response.text())
+                    r = await response.json()
                     return r["result"]
                 except Exception as e:
                     print(e)
                     return "API is currently offline or down. Please try again later."
                 
+class DeloreanOwner():
+    async def get(self, url, api_key,advanced_mode=True):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"http://localhost:4685/?url={quote(url)}", headers={"x-api-key": api_key}) as response:
+                if response.status != 200:
+                    return await response.text()
+                try:
+                    r = await response.json()
+                    if advanced_mode:
+                        return r
+                    return r["result"]
+                except Exception as e:
+                    print(e)
+                    return "API is currently offline or down. Please try again later."       
